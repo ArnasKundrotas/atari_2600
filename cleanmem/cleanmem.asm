f334a9d14c2c97c251100dcd7a0462d011339a90
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-08-18 20:00:00 +0300

    Clear the pager zero region start

--- cleanmem/cleanmem.asm
+++ cleanmem/cleanmem.asm
@@ -4,6 +4,12 @@
     org $F000   ; Define the code origin at $F000
 
 Start:
-    
+    sei         ; Disable interrupts
+    cld         ; Disable the BCD decimal math mode
+    ldx #$FF    ; Loads the X register with #$FF
+    txs         ; Transfer the X register to the (S)tack pointer
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Clear the Pager Zero region ($00 ti $FF)
+; Meaning the entire RAM and also the entire TIA register
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
