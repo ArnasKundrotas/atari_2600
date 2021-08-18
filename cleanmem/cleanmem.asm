14fc45dcf1a3aade36d7160cfb65c03378c3e164
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-08-18 20:12:08 +0300

    Clear the page zero region end

--- cleanmem/cleanmem.asm
+++ cleanmem/cleanmem.asm
@@ -1,26 +1,21 @@
     processor 6502
-
     seg code
     org $F000           ; Define the code origin at $F000
-
 Start:
     sei                 ; Disable interrupts
     cld                 ; Disable the BCD decimal math mode
     ldx #$FF            ; Loads the X register with #$FF
     txs                 ; Transfer the X register to the (S)tack pointer
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Clear the Page Zero region ($00 ti $FF)
 ; Meaning the entire RAM and also the entire TIA register
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     lda #0              ; A = 0
     ldx #$FF            ; X = #$FF
-
 MemLoop:
     sta $0,X            ; Store the value of A inside memory address $0 + X
     dex                 ; X--
     bne MemLoop         ; Loop until X = 0 ( z-flag is set )
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Fill the ROM size to exactly 4KB
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
