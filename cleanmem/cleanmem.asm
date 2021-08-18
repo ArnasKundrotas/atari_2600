5b79ba65cd183a372386b2f2a71e4b0f06abc2df
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-08-18 20:11:05 +0300

    Clear the page zero region end

--- cleanmem/cleanmem.asm
+++ cleanmem/cleanmem.asm
@@ -1,15 +1,29 @@
     processor 6502
 
     seg code
-    org $F000   ; Define the code origin at $F000
+    org $F000           ; Define the code origin at $F000
 
 Start:
-    sei         ; Disable interrupts
-    cld         ; Disable the BCD decimal math mode
-    ldx #$FF    ; Loads the X register with #$FF
-    txs         ; Transfer the X register to the (S)tack pointer
+    sei                 ; Disable interrupts
+    cld                 ; Disable the BCD decimal math mode
+    ldx #$FF            ; Loads the X register with #$FF
+    txs                 ; Transfer the X register to the (S)tack pointer
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Clear the Page Zero region ($00 ti $FF)
 ; Meaning the entire RAM and also the entire TIA register
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #0              ; A = 0
+    ldx #$FF            ; X = #$FF
+
+MemLoop:
+    sta $0,X            ; Store the value of A inside memory address $0 + X
+    dex                 ; X--
+    bne MemLoop         ; Loop until X = 0 ( z-flag is set )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Fill the ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $FFFC
+    .word Start         ; reset vector at $FFFC ( ehre the program starts )
+    .word Start         ; Interrupt vector at $FFFE ( unused in the VCS )
